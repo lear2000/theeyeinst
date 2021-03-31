9b68219fcddaf939698f3a76f70d9c35afd88de8
Matt <matt@firesuitemultimedia.com>
2021-03-31 11:03:38 -0700

    wrap

--- tmpl/snippet-header.php
+++ tmpl/snippet-header.php
@@ -1,11 +1,25 @@
 <header>
 	<a href="#" class="logo"><img src="img/logo.svg" alt="Logo" width="173.087" height="48.026"/></a>
-	<nav class="nav-top">
-		<ul>
-			<li><a>Current COVID-19 Safety Protocols</a></li>
-			<li><a>Other Services</a></li>
-			<li><a>Patient Info</a></li>
-			<li><a>Research</a></li>
-		</ul>
-	</nav>
+	<div class="nav-wraps">
+		<nav class="nav-top">
+			<ul>
+				<li><a>Current COVID-19 Safety Protocols</a></li>
+				<li><a>Other Services</a></li>
+				<li><a>Patient Info</a></li>
+				<li><a>Research</a></li>
+			</ul>
+		</nav>
+		<nav class="nav-bottom">
+			<ul>
+				<li><a href="">About</a></li>
+				<li><a href="">LASIK</a></li>
+				<li><a href="">LASIK Alternatives</a></li>
+				<li><a href="">Cataracts</a></li>
+				<li><a href="">Dry Eye</a></li>
+				<li><a href="">Vision 40+</a></li>
+				<li><a href="">Patient Stories</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
 </header>
