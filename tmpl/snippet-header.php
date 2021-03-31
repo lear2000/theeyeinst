755e76243998f8acc865542eb201ae6a7aba2b13
Matt <matt@firesuitemultimedia.com>
2021-03-31 12:02:31 -0700

    main nav deskt

--- tmpl/snippet-header.php
+++ tmpl/snippet-header.php
@@ -15,10 +15,25 @@
 				<li><a href="">LASIK</a></li>
 				<li><a href="">LASIK Alternatives</a></li>
 				<li><a href="">Cataracts</a></li>
-				<li><a href="">Dry Eye</a></li>
+				<li class="menu-item-has-children">
+						<a href="">Dry Eye</a>
+						<ul class="sub-menu depth-0">
+							<li><a>Dropdown1</a></li>
+							<li><a>Dropdown2</a></li>
+							<li><a>Dropdown3</a></li>
+							<li><a>Dropdown4</a></li>
+							<li class="menu-item-has-children">
+								<a>Dropdown5</a>
+								<ul class="sub-menu depth-1">
+									<li><a href="">Grandchild</a></li>
+									<li><a href="">Grandchild</a></li>
+								</ul>
+							</li>
+						</ul>
+				</li>
 				<li><a href="">Vision 40+</a></li>
 				<li><a href="">Patient Stories</a></li>
-				<li><a href="">Contact</a></li>
+				<li class="menu-item-has-children"><a href="">Contact</a></li>
 			</ul>
 			<a class="h-tel" href="tel:(801) 980-3592">(801) 980-3592</a>
 		</nav>
