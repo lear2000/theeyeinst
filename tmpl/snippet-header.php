6154debc7bfe33b3cf44dcb60fc85baad1a312e5
Matt <matt@firesuitemultimedia.com>
2021-03-31 13:01:13 -0700

    mob nav start

--- tmpl/snippet-header.php
+++ tmpl/snippet-header.php
@@ -1,6 +1,11 @@
+<div class="banner">Current COVID-19 Safety Protocols</div>
 <header>
 	<a href="#" class="logo"><img src="img/logo.svg" alt="Logo" width="173.087" height="48.026"/></a>
 	<div class="nav-wraps">
+		<div class="burger">
+			<span class="bar1"></span>
+			<span class="bar2"></span>
+		</div>
 		<nav class="nav-top">
 			<ul>
 				<li><a>Current COVID-19 Safety Protocols</a></li>
