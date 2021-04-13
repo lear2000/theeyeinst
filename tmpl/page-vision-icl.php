592e000c7192725b21b9b9f4ae605d8480fc5fb6
Ruben.M <rmarin@miva.com>
2021-04-12 21:39:17 -0700

    vision icl page started

--- tmpl/page-vision-icl.php
+++ tmpl/page-vision-icl.php
@@ -7,6 +7,18 @@
 </section>
 
 
+<section class="layout-1-2-alternate">
+	<div class="layout-1-2-alternate__wrapper layout-container">
+		<div class="layout-1-2-alternate__item layout-1-2-alternate__image">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/vision-wireframe.png" alt="{replace me}">
+		</div>
+		<div class="layout-1-2-alternate__item layout-1-2-alternate__text">
+			<p>If your doctor said you weren’t a good candidate for LASIK or PRK because you had too thin corneas, dry eyes, or your prescription was out of range, then the Visian ICL may be just perfect for you! In addition, the Visian ICL is also available to people who are looking for other alternatives to LASIK. The Visian ICL is an implantable contact/collamer lens. It’s different from LASIK because it doesn’t involve a laser to treat the cornea. The Visian ICL is actually an implant that works in harmony with your natural eye! The surgeon will make a tiny incision and insert a microthin contact lens in front of the eye’s natural lens inside the eye. You will never have to worry again about foggy glasses or losing a contact. Our ICL patients are thrilled with their results – in the F.D.A. clinical trials, patients reported a 99% satisfaction rate!</p>
+		</div>
+	</div>
+</section>
+
+
 <section class="layout-1-2 layout-1-2-reverse layout-1-2-type__inner">
 	<div class="layout-1-2__image layout-1-2-reverse__image" style="background-image:url(<?php echo get_template_directory_uri( )?>/img/icl-stock-img.jpg);"></div>
 	<div class="layout-1-2__text layout-1-2-reverse__text">
@@ -15,6 +27,43 @@
 	</div>
 </section>
 
+
+<section class="layout-1-2-video">
+	<div class="layout-1-2-video__wrapper layout-container">
+		
+		<div class="layout-1-2-video__item layout-1-2-video__text">
+			<h3 class="layout-1-2-video__text-heading is-heading">The Visian ICL Experience</h3>
+			<p class="layout-1-2-video__text-text">Lori’s experience with her Visian ICL procedure.  Lipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
+		</div>	
+		<div class="layout-1-2-video__item layout-1-2-video__video">
+			<div class="layout-1-2-video__video-object">
+				<img src="<?php echo get_template_directory_uri();?>/img/video-dummy.jpg" alt="{replace me}">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="layout-1-2-video layout-1-2-video-reverse">
+	<div class="layout-1-2-video__wrapper layout-container">
+		
+		<div class="layout-1-2-video__item layout-1-2-video__text">
+			<h3 class="layout-1-2-video__text-heading is-heading">How Does It Work?</h3>
+			<p class="layout-1-2-video__text-text">Lori’s experience with her Visian ICL procedure.  Lipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
+		</div>	
+		<div class="layout-1-2-video__item layout-1-2-video__video">
+			<div class="layout-1-2-video__video-object">
+				<img src="<?php echo get_template_directory_uri();?>/img/video-dummy.jpg" alt="{replace me}">
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="layout-text-only">
+	<div class="layout-text-only__wrapper layout-container">
+		<p>In addition, the benefit of the Visian ICL and Visian Toric ICL include a short recovery time, minimal risks and complications, removable, and UV blocking. If you would like to see how the Visian ICL or Visian Toric ICL can benefit your lifestyle, schedule a complimentary evaluation today at 801-980-0640 and take advantage of this great technology!</p>
+	</div>
+</section>
 
 
 <?php //include 'layout-1-2.php'; ?>
