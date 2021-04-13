0a5293130eefebbae8536586f3e722d93d910d7f
Matt <matt@firesuitemultimedia.com>
2021-04-13 09:14:25 -0700

    blog

---
+++ blog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php include 'tmpl/snippet-header.php'; ?>
+	<header class="wideheader">
+		<div class="wh-bg" style="background-image: url(img/wideheader.jpg)"></div>
+	</header>
+	<div class="fake">
+	<div class="titlebar">
+		<h1>Blog</h1>
+	</div>
+	</div>
+
+
+
+		<!-- scripts	 -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="js/scripts-min.js"></script>
+</body>
+</html>
