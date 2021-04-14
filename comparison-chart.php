1b77042a542f2ef271b3fecfd3c2744844a13a2a
Matt <matt@firesuitemultimedia.com>
2021-04-13 18:02:03 -0700

    comp chart

---
+++ comparison-chart.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+<section class="compchart">
+	<h2>Comparison Chart</h2>
+	<p>See how the most popular vision correction options compare.</p>
+
+	<div class="cgrid-wrap">
+		<div class="cgrid bgwhite">
+			&nbsp;
+		</div>
+
+		<div class="cgrid bgdark">
+			<img src="img/visian-grid.svg" />
+		</div>
+	<div class="cnote-wrap">
+		<div class="cgrid cnotes">
+			<span><div>Visual Clarity</div></span>
+			<span><div>Excellent</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Patient Satisfaction</div></span>
+			<span><div>High</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Outpatient Procedure</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Procedure Duration</div></span>
+			<span><div>15-20 minutes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Recovery Time</div></span>
+			<span><div>Days</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Reversible or Removable</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>UV Protection</div></span>
+			<span><div>Yes</div></span>
+		</div>
+	</div>
+
+
+		<div class="cgrid bglight">
+			<img src="img/lasik-grid.svg" />
+		</div>
+		<div class="cnote-wrap">
+		<div class="cgrid cnotes">
+			<span><div>Visual Clarity</div></span>
+			<span><div>Excellent</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Patient Satisfaction</div></span>
+			<span><div>High</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Outpatient Procedure</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Procedure Duration</div></span>
+			<span><div>15-20 minutes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Recovery Time</div></span>
+			<span><div>Days</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Reversible or Removable</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>UV Protection</div></span>
+			<span><div>Yes</div></span>
+		</div>
+	</div>
+
+
+
+
+		<div class="cgrid bgdark">
+			<img src="img/prk-grid.svg" />
+		</div>
+	<div class="cnote-wrap">
+		<div class="cgrid cnotes">
+			<span><div>Visual Clarity</div></span>
+			<span><div>Excellent</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Patient Satisfaction</div></span>
+			<span><div>High</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Outpatient Procedure</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Procedure Duration</div></span>
+			<span><div>15-20 minutes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Recovery Time</div></span>
+			<span><div>Days</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Reversible or Removable</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>UV Protection</div></span>
+			<span><div>Yes</div></span>
+		</div>
+	</div>
+
+
+
+		<div class="cgrid bglight no-b">
+			<img src="img/rle-grid.svg" />
+		</div>
+	<div class="cnote-wrap">
+		<div class="cgrid cnotes">
+			<span><div>Visual Clarity</div></span>
+			<span><div>Excellent</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Patient Satisfaction</div></span>
+			<span><div>High</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Outpatient Procedure</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Procedure Duration</div></span>
+			<span><div>15-20 minutes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Recovery Time</div></span>
+			<span><div>Days</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>Reversible or Removable</div></span>
+			<span><div>Yes</div></span>
+		</div>
+		<div class="cgrid cnotes">
+			<span><div>UV Protection</div></span>
+			<span><div>Yes</div></span>
+		</div>
+	</div>
+
+	<div class="d-gridwrap">
+		<span>Visual Clarity</span>
+		<span>Excellent</span>
+		<span>Excellent</span>
+		<span>Excellent</span>
+		<span>Excellent</span>
+	</div>
+	<div class="d-gridwrap">
+		<span>Visual Clarity</span>
+		<span>Excellent</span>
+		<span>Excellent</span>
+		<span>Excellent</span>
+		<span>Excellent</span>
+	</div>
+
+	</div>
+
+
+
+</section>
+
+
+		<!-- scripts	 -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="js/scripts-min.js"></script>
+</body>
+</html>
