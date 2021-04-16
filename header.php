571625bb72152cb9c625f634d5f491cace10849e
Matt <matt@firesuitemultimedia.com>
2021-04-16 11:33:17 -0700

    update home page layouts

--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <head>
 	<title><?php wp_title(); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory');?>/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('template_directory');?>/favicon/android-icon-192x192.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory');?>/favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_directory');?>/favicon/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory');?>/favicon/favicon-16x16.png">
